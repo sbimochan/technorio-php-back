a96b708929af8262a6ffd6505fef9b586da60083
Bimochan Shrestha <bmochan@gmail.com>
2017-02-02 17:52:52 +0000

    rolling back

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,57 +1,50 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+    <div class="container">
+      <div class="row">
+          <div class="col-md-8 col-md-offset-2">
 
 
-                <h1>Post an Article</h1><hr>
+                  <h1>Post an Article</h1><hr>
 
- @if (count($errors) > 0)
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
+                  @if (count($errors) > 0)
+                   <div class="alert alert-danger">
+                      <ul>
+                        @foreach ($errors->all() as $error)
+                          <li>{{ $error }}</li>
+                        @endforeach
+                      </ul>
+                    </div>
+                          
+                  @endif
+
+
+                <form class="" action="posts" method="post" onsubmit="myValidate()">
+                  <div class="form-group">
+                    <label for="title">Title</label>
+                      <input type="text" name="title" id="title" value="" class="form-control">
+                  </div>
+                  <div class="form-group">
+                    <label for="content">Content</label>
+                    <textarea name="content" id="content"></textarea>
+                  </div>
+                   <div class="form-group">
+                    <label for="tags">tags</label>
+                      <input type="text" name="tags" id="tags" value="" class="form-control">
+                  </div>
+                
+
+                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                    <br><br><br>
+                  <div class="form-group">
+                    <input type="submit" name="btn_submit" class="btn btn-primary" value="Post">
+                  </div>
+                </form>
+                <div>
+                 <a href="{{ URL::to('article') }}" class="btn btn-success pull-right" >Article</a>
+                </div>
+          </div>
+      </div>
     </div>
-}
-}
-@endif
-
-
-            <form class="" action="posts" method="post">
-              <div class="form-group">
-                <label for="title">Title</label>
-                <input type="text" name="title" id="title" value="" class="form-control">
-              </div>
-              <div class="form-group">
-                <label for="title">Content</label>
-                <textarea name="content" id="content"></textarea>
-              </div>
-              <div id="tags">
-              <span>php</span>
-               <span>c++</span>
-               <span>jquery</span>
-               
-                <input type="text" name="tags" id="tags" value="" class="form-control" placeholder="Add a tag">
-              </div>
-              
-
-              <input type="hidden" name="_token" value="{{ csrf_token() }}">
-              <br><br><br>
-              <div class="form-group">
-                <input type="submit" name="btn_submit" class="btn btn-primary" value="Post">
-              </div>
-            </form>
-<div>
- <a href="{{ URL::to('article') }}" class="btn btn-success pull-right" >Article</a>
-</div>
-        </div>
-    </div>
-</div>
-
-
-
 @endsection
